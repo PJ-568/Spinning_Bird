2b89b54c0bdcb93a913b7938d118a10d072e0372
PJ568 <PJ568@PJ568.eu.org>
2024-04-09 10:58:25 +0800

    [修复] 翻译问题

--- js/translate_lib.js
+++ js/translate_lib.js
@@ -1,10 +1,14 @@
-try{
-    translate.listener.start();
-    translate.language.setLocal('chinese_simplified');
-    translate.setAutoDiscriminateLocalLanguage();
-    translate.language.setUrlParamControl();
+function initTranslate() {
+    try{
+        translate.listener.start();
+        translate.language.setLocal('chinese_simplified');
+        translate.setAutoDiscriminateLocalLanguage();
+        translate.language.setUrlParamControl();
+    }
+    catch(e){console.log(e);}
+    translate.setUseVersion2();
+    translate.ignore.class.push('notTranslate');
+    translate.execute();
 }
-catch(e){console.log(e);}
-translate.setUseVersion2();
-translate.ignore.class.push('notTranslate');
-translate.execute();
+
+window.addEventListener('load', initTranslate);
